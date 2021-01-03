f163842de925e32018ae9844d1c848a1eb772948
Mateusz Kuzmik <mateusz.kuzmik@allegro.pl>
2021-01-03 16:49:06 +0100

    Strava matchers

--- background.js
+++ background.js
@@ -4,9 +4,13 @@ chrome.runtime.onInstalled.addListener(function () {
     });
     chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
         chrome.declarativeContent.onPageChanged.addRules([{
-            conditions: [new chrome.declarativeContent.PageStateMatcher({
-                pageUrl: {hostEquals: 'developer.chrome.com'},
-            })
+            conditions: [
+                new chrome.declarativeContent.PageStateMatcher({
+                    pageUrl: {hostEquals: 'www.strava.com'},
+                }),
+                new chrome.declarativeContent.PageStateMatcher({
+                    pageUrl: {hostEquals: 'strava.com'},
+                })
             ],
             actions: [new chrome.declarativeContent.ShowPageAction()]
         }]);
